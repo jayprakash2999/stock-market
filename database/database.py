31dca3a4edc73126503b36cf32a2f5eb96b15f0c
Jay Prakash <jay.prakash@bridgeweave.com>
2024-01-08 06:22:26 +0530

    Bug Fixes

--- database/database.py
+++ database/database.py
@@ -3,8 +3,10 @@ from database.config import DB_CONFIG
 
 
 def get_database():
+    connection = None
     try:
         connection = connect(**DB_CONFIG)
         yield connection
     finally:
-        connection.close()
+        if connection is not None:
+            connection.close()
